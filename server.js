f0d6db84e3acc739f7507662831d004a15b596d5
KhangItWebDev <khang01279567920@gmail.com>
2022-12-09 15:34:27 +0700

    init source

---
+++ server.js
@@ -0,0 +1,15 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3200;
+
+server.use(middlewares);
+server.use(router);
+server.use(jsonServer.bodyParser);
+server.post("*", (req, res) => {
+  if (req.method === "POST") {
+    res.status(200).jsonp();
+  }
+});
+server.listen(port);
